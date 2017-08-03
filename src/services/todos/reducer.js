1ed7b02da0186ed113afea894a19e53bd85f0029
Masu Lin <masu.lin@104.com.tw>
2017-08-03 15:23:02 +0800

    #30

--- src/services/todos/reducer.js
+++ src/services/todos/reducer.js
@@ -1,3 +1,7 @@
+import {
+    types,
+} from './actions';
+
 const INITIAL_STATE = [
   {
     text: 'Use Redux',
@@ -7,5 +11,19 @@ const INITIAL_STATE = [
 ]
 
 export default (state = INITIAL_STATE, action) => {
-    return state;
+    const { ADD_TODO } = types;
+    switch (action.type) {
+        case ADD_TODO:
+            return [
+                {
+                    id: state.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1,
+                    completed: false,
+                    text: action.text
+                },
+                ...state
+            ];
+        default:
+            return state;
+    }
 }
+  
