ce60963226992b7a9885051f5f9f89403100e387
Masu Lin <masu.lin@104.com.tw>
2017-08-04 00:03:47 +0800

    #55 atom src/services/todos/actions/index.js

--- src/services/todos/actions/index.js
+++ src/services/todos/actions/index.js
@@ -1,11 +1,21 @@
 import * as types from './types'
 
+import api from '../api';
+
 const addTodo = text => ({ type: types.ADD_TODO, text })
 const deleteTodo = id => ({ type: types.DELETE_TODO, id })
 const editTodo = (id, text) => ({ type: types.EDIT_TODO, id, text })
 const completeTodo = id => ({ type: types.COMPLETE_TODO, id })
 const completeAll = () => ({ type: types.COMPLETE_ALL })
 const clearCompleted = () => ({ type: types.CLEAR_COMPLETED })
+
+const initTodos = () => dispatch => {
+    return api.get().then((todos) => {
+        dispatch(updateTodos(todos));
+    });
+}
+
+const updateTodos = (todos) => ({ type: types.UPDATE_TODOS, todos})
 
 export {
     types,
@@ -15,4 +25,6 @@ export {
     completeTodo,
     completeAll,
     clearCompleted,
+    initTodos,
+    updateTodos,
 }
