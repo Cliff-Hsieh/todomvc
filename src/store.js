60b464c8c124c894ef05630369c9e08d6d333b22
Masu Lin <masu.lin@104.com.tw>
2017-08-04 00:00:54 +0800

    #53 atom src/store.js

--- src/store.js
+++ src/store.js
@@ -1,7 +1,20 @@
-import { createStore } from 'redux'
+import {
+    compose,
+    createStore,
+    applyMiddleware
+} from 'redux'
 import devToolsEnhancer from 'remote-redux-devtools';
+import thunk from 'redux-thunk';
 import reducer from './reducer';
 
-const store = createStore(reducer, devToolsEnhancer());
+const store = createStore(
+    reducer,
+    compose(
+        applyMiddleware(
+            thunk,
+        ),
+        devToolsEnhancer()
+    )
+);
 
 export default store;
