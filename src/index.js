4cb22dfefd6004fc2ce93726e9f74d0c70337cc9
Masu Lin <masu.lin@104.com.tw>
2017-08-03 14:58:52 +0800

    #14

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/app'
+import 'todomvc-app-css/index.css'
+
+import { Provider } from 'react-redux'
+import store from './store';
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
