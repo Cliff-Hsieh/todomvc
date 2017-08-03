6cfb4f060ef22c40c98b9bf9829dbc57d04f53e1
Masu Lin <masu.lin@104.com.tw>
2017-08-03 15:36:35 +0800

    #46

--- src/services/todos/actions/index.js
+++ src/services/todos/actions/index.js
@@ -1,8 +1,18 @@
 import * as types from './types'
 
 const addTodo = text => ({ type: types.ADD_TODO, text })
+const deleteTodo = id => ({ type: types.DELETE_TODO, id })
+const editTodo = (id, text) => ({ type: types.EDIT_TODO, id, text })
+const completeTodo = id => ({ type: types.COMPLETE_TODO, id })
+const completeAll = () => ({ type: types.COMPLETE_ALL })
+const clearCompleted = () => ({ type: types.CLEAR_COMPLETED })
 
 export {
     types,
     addTodo,
+    deleteTodo,
+    editTodo,
+    completeTodo,
+    completeAll,
+    clearCompleted,
 }
