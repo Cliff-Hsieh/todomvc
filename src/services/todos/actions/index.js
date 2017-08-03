9e527484678c7ee9c77bdcb82e50cd811f94da81
Masu Lin <masu.lin@104.com.tw>
2017-08-04 00:20:35 +0800

    #58 atom src/services/todos/actions/index.js

--- src/services/todos/actions/index.js
+++ src/services/todos/actions/index.js
@@ -2,7 +2,23 @@ import * as types from './types'
 
 import api from '../api';
 
-const addTodo = text => ({ type: types.ADD_TODO, text })
+const addTodo = text => dispatch => {
+    api.get().then((todos) => {
+        const newTodos = todos.concat(
+            {
+                id: todos.reduce(
+                        (maxId, todo) => Math.max(todo.id, maxId), -1
+                    ) + 1,
+                completed: false,
+                text: text
+            }
+        );
+
+        api.update(newTodos)
+            .then(() => dispatch(updateTodos(newTodos)));
+    });
+}
+
 const deleteTodo = id => ({ type: types.DELETE_TODO, id })
 const editTodo = (id, text) => ({ type: types.EDIT_TODO, id, text })
 const completeTodo = id => ({ type: types.COMPLETE_TODO, id })
