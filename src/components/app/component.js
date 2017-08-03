e828352af9e701f63ad00d3ebc36b9ee28301b54
Masu Lin <masu.lin@104.com.tw>
2017-08-03 15:35:09 +0800

    #44

--- src/components/app/component.js
+++ src/components/app/component.js
@@ -2,9 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Header from '../Header';
+import MainSection from '../MainSection';
 
 export default () => (
   <div>
       <Header />
+      <MainSection />
   </div>
 )
