7d717f5a08164ccb179323cb4949200fa0e5f315
Masu Lin <masu.lin@104.com.tw>
2017-08-04 00:06:33 +0800

    #57 atom src/index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import 'todomvc-app-css/index.css'
 
 import { Provider } from 'react-redux'
 import store from './store';
+import * as actions from './actions';
+
+store.dispatch(actions.services.todos.initTodos());
 
 render(
   <Provider store={store}>
